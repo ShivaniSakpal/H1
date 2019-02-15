b883f5488ecca33216c921acc6f01aa8a4e4dd2b
ShivaniSakpal <46843290+ShivaniSakpal@users.noreply.github.com>
2019-02-15 10:58:53 +0530

    Delete donuts.py

--- donuts.py
+++
@@ -1,6 +0,0 @@
-#WAP to accept number of donuts as an input,if number of donuts are greater than 10 then print "No of donuts : Many" else print "No of remanining donuts:n".
-n=int(input("Enter number of donuts"))
-if(n>10):
-	print("Number of donuts : Many")
-else:
-	print("Number of donuts",n)
