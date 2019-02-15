2dd3e6e08ea66525dbf9022ce8cba731ef6dd667
ShivaniSakpal <46843290+ShivaniSakpal@users.noreply.github.com>
2019-02-15 11:00:13 +0530

    Delete table.py

--- table.py
+++
@@ -1,8 +0,0 @@
-#WAP to accept an integer and print tables from it till 2 to n
-n=int(input("Enter a number"))
-for j in range(2,n+1):
-	print("Table of",j)
-	for i in range(11):
-		
-		print(j,'X',i,'=',(n*i))
-		
