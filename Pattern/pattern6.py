afd9a69d7f0398d01d63fef5897910383662ab94
ShivaniSakpal <46843290+ShivaniSakpal@users.noreply.github.com>
2019-02-15 20:42:48 +0530

    Add files via upload

---
+++ Pattern/pattern6.py
@@ -0,0 +1,19 @@
+def pat(n):
+	for _ in range(1,2*n):
+		print("*",end=" ")
+	print()	
+	
+	for i in range(1,2*n):
+		for _ in range(1,n-i+1):
+			print("*",end=' ')
+		for _ in range(1,i*2):
+			print(" ",end=' ')
+		for _ in range(1,n-i+1):
+			print("*",end=' ')
+		print()
+def main():
+	n=int(input("Enter Number of Rows\n"))
+	pat(n)
+	
+if __name__=='__main__':
+	main()
